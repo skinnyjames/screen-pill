8b68e3269ee73379b73975fcddcf64c338fae32f
Sean Gregory <seangregory@protonmail.com>
2018-08-30 14:50:13 -0400

    adding lib for shared functions

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
+const Elements = require('./lib/elements')
+
 module.exports = function(driver) {
-
-  var Elements = { 
-    element: function() { 
-      return this.driver.findElement(this.identifier) 
-    },
-    waitUntil: function(cb, timeout=5000, message='wait condition not met') {
-      let element = this.driver.findElement(this.identifier)
-      let condition = function() {
-        return cb(element)
-      }
-      return this.driver.wait(condition, timeout, message)
-    },
-    waitUntilPresent: function(timeout=5000, message='element not present') {
-      return this.driver.wait(() => {
-        return this.driver.findElement(this.identifier).then(success, error)
-
-        function success(element) {
-          return true
-        }
-
-        function error(err) {
-          return false
-        }
-      }, timeout, message)
-    }
-  }
 
   return function(PageClass) {
 
@@ -58,6 +34,9 @@ module.exports = function(driver) {
     
 
     this.div = function PageObject$div (key, identifier) {
+
+      // more specific identifier
+      identifier['tagName'] = 'div'
 
       self[key] = {
         get: function() {
