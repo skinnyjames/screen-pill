258a6a9718b4bb6571bb06e164ae800d7749d9d1
Sean Gregory <seangregory@protonmail.com>
2018-09-03 09:55:20 -0400

    whoops

--- page-factory.js
+++ page-factory.js
@@ -1,11 +1,11 @@
 module.exports = function(PageClass) {
   return {
     on: function(PageClass, cb) {
-      let pc = PageClass.new(this.driver)
+      let pc = new PageClass(this.driver)
       return cb(pc)
     },
     visit: async function(PageClass, cb) {
-      let pc = PageClass.new(this.driver)
+      let pc = new PageClass(this.driver)
       await pc.visit()
       return cb(pc)
     }
