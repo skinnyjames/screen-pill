a41e19bf3c82177f7499d0dba10382cd40eaf194
Sean Gregory <seangregory@protonmail.com>
2018-09-03 10:07:18 -0400

    page-factory object

--- page-factory.js
+++ page-factory.js
@@ -1,17 +1,15 @@
-module.exports = function(PageClass) {
-  return {
-    on: function(PageClass, cb) {
-      let pc = new PageClass(this.driver)
-      if (cb && typeof cb == 'function') {
-        return cb(pc)
-      }
-    },
-    visit: async function(PageClass, cb) {
-      let pc = new PageClass(this.driver)
-      await pc.visit()
-      if (cb && typeof cb == 'function') {
-        return cb(pc)
-      }
+module.exports = {
+  on: function(PageClass, cb) {
+    let pc = new PageClass(this.driver)
+    if (cb && typeof cb == 'function') {
+      return cb(pc)
+    }
+  },
+  visit: async function(PageClass, cb) {
+    let pc = new PageClass(this.driver)
+    await pc.visit()
+    if (cb && typeof cb == 'function') {
+      return cb(pc)
     }
   }
 }
